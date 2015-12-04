8bd3bd9fa799a9d2bf584b2555c3e775022e4dec
Ernst Haagsman <ernsthaagsman@gmail.com>
2015-12-03 16:26:11 -0800

    Run recording

--- app/src/main/java/cmpe277/project/skibuddy/RunActivity.java
+++ app/src/main/java/cmpe277/project/skibuddy/RunActivity.java
@@ -1,18 +1,30 @@
 package cmpe277.project.skibuddy;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class RunActivity extends FragmentActivity implements OnMapReadyCallback {
+import cmpe277.project.skibuddy.common.Run;
+import cmpe277.project.skibuddy.common.SkiBuddyLocation;
+import cmpe277.project.skibuddy.server.ServerSingleton;
+
+public class RunActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private Button toggleRecordButton;
+    private Run currentRun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +33,65 @@ public class RunActivity extends FragmentActivity implements OnMapReadyCallback 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        LocationManager locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        } catch (SecurityException e) {
+            Toast t = Toast.makeText(this, "Please enable location services", Toast.LENGTH_LONG);
+            t.show();
+        }
         mapFragment.getMapAsync(this);
+
+        final Context context = this;
+
+        toggleRecordButton = (Button)findViewById(R.id.toggle_record_button);
+        toggleRecordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentRun == null){
+                    // Not recording, so start recording
+                    currentRun = ServerSingleton.createRun();
+                    toggleRecordButton.setText("STOP");
+                } else {
+                    Run runToStore = currentRun;
+                    currentRun = null;
+                    toggleRecordButton.setText("REC");
+                    Toast t = Toast.makeText(context, String.format("Run distance: %.2f m", runToStore.getDistance()), Toast.LENGTH_LONG);
+                    t.show();
+                }
+            }
+        });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onLocationChanged(Location location) {
+        if (currentRun != null) {
+            SkiBuddyLocation newLocation = ServerSingleton.createLocation();
+            newLocation.setElevation(location.getAltitude());
+            newLocation.setLatitude(location.getLatitude());
+            newLocation.setLongitude(location.getLongitude());
+            currentRun.extendTrack(newLocation);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
