d2693e6740dc8e54ffb6a14a4888f423d0a53782
Nergal <nergal.issaie@hds.com>
2015-12-06 00:47:48 -0800

    user profile is created and the login is connected to it

--- app/src/main/java/cmpe277/project/skibuddy/SignInActivity.java
+++ app/src/main/java/cmpe277/project/skibuddy/SignInActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,18 +18,68 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.common.api.Status;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+//import com.google.api.services.bigquery.BigqueryScopes;
+//import com.google.api.services.drive.Drive;
+//import com.google.api.services.drive.DriveScopes;
+//import com.google.api.services.oauth2.Oauth2;
+//import com.google.api.services.oauth2.model.Userinfoplus;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.services.oauth2.Oauth2;
+//import com.google.api.services.oauth2.model.Userinfoplus;
+//import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+//import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.*;
+
+import cmpe277.project.skibuddy.common.User;
 
 public class SignInActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener,
@@ -43,6 +94,39 @@ public class SignInActivity extends AppCompatActivity implements
     private ImageView imageView;
     private TextView tagline;
 
+    ///
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+            "Drive API Java Quickstart";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+            JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart. */
+//    private static final List<String> SCOPES =
+//            Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
+
+/*    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +136,7 @@ public class SignInActivity extends AppCompatActivity implements
 
         ///
         imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageBitmap(null);
         tagline = (TextView)findViewById(R.id.tagline);
         ///
 
@@ -66,11 +151,15 @@ public class SignInActivity extends AppCompatActivity implements
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .addScope(new Scope(Scopes.PROFILE))
+                .addScope(new Scope(Scopes.EMAIL))
+                .addApi(Plus.API) ///
                 .build();
 
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
         signInButton.setScopes(gso.getScopeArray());
+
     }
 
     @Override
@@ -81,13 +170,16 @@ public class SignInActivity extends AppCompatActivity implements
         if (opr.isDone()) {
             Log.d(TAG, "Got cached sign-in");
             GoogleSignInResult result = opr.get();
-            try {
-                handleSignInResult(result);
+
+/*            try {
+                ;//handleSignInResult(result);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
-            }
+            } catch (NoUserIdException e) {
+                e.printStackTrace();
+            }*/
         } else {
             showProgressDialog();
             opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
@@ -99,6 +191,8 @@ public class SignInActivity extends AppCompatActivity implements
                     } catch (IOException e) {
                         e.printStackTrace();
                     } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (NoUserIdException e) {
                         e.printStackTrace();
                     }
                 }
@@ -112,36 +206,159 @@ public class SignInActivity extends AppCompatActivity implements
 
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
             try {
                 handleSignInResult(result);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
+            } catch (NoUserIdException e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private void handleSignInResult(GoogleSignInResult result) throws IOException, JSONException {
+    /**
+     * Exception thrown when no user ID could be retrieved.
+     */
+    private static class NoUserIdException extends Exception {
+    }
+
+    /**
+     * Exception thrown when a code exchange has failed.
+     */
+    public static class CodeExchangeException extends GetCredentialsException {
+        /**
+         * Construct a CodeExchangeException.
+         *
+         * @param authorizationUrl The authorization URL to redirect the user to.
+         */
+        public CodeExchangeException(String authorizationUrl) {
+            super(authorizationUrl);
+        }
+    }
+  /*  *//**
+     * Build an authorization flow and store it as a static class attribute.
+     *
+     * @return GoogleAuthorizationCodeFlow instance.
+     * @throws IOException Unable to load client_secret.json.
+     *//*
+    static GoogleAuthorizationCodeFlow getFlow() throws IOException {
+        if (flow == null) {
+            InputStream in =
+                    MyClass.class.getResourceAsStream(CLIENTSECRET_LOCATION);
+            GoogleClientSecrets clientSecret =
+                    GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+            flow = new GoogleAuthorizationCodeFlow.Builder(
+                    HTTP_TRANSPORT, JSON_FACTORY, clientSecret, SCOPES)
+                    .setAccessType("offline")
+                    .setApprovalPrompt("force")
+                    .build();
+        }
+        return flow;
+    }
+    private static Credential authorize() throws Exception {
+        // load client secrets
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+                new InputStreamReader(PlusSample.class.getResourceAsStream("/client_secrets.json")));
+        // set up authorization code flow
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, JSON_FACTORY, clientSecrets,
+                Collections.singleton(PlusScopes.PLUS_ME)).setDataStoreFactory(
+                dataStoreFactory).build();
+        // authorize
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    }
+
+    /**
+     * Exchange an authorization code for OAuth 2.0 credentials.
+     *
+     * @param code to exchange for OAuth 2.0
+     *     credentials.
+     * @return OAuth 2.0 credentials.
+     * @throws CodeExchangeException An error occurred.
+     */
+    /*
+    static Credential exchangeCode(String authorizationCode)
+            throws CodeExchangeException {
+        try {
+            GoogleAuthorizationCodeFlow flow = getFlow();
+            GoogleTokenResponse response = flow
+                    .newTokenRequest(authorizationCode)
+                    .setRedirectUri(REDIRECT_URI)
+                    .execute();
+            return flow.createAndStoreCredential(response, null);
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e);
+            throw new CodeExchangeException(null);
+        }
+    }*/
+
+    private void handleSignInResult(GoogleSignInResult result) throws IOException, JSONException, NoUserIdException {
+        imageView.setImageBitmap(null);
         Log.d(TAG, "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
+            ///
+            //UserProfileActivity userProfile = new UserProfileActivity(mGoogleApiClient);
+            //Intent userProfileIntent = userProfile.getIntent();
+
+            Intent userProfileIntent = new Intent(SignInActivity.this, UserProfileActivity.class);
+            ArrayList<String> arrStr = new ArrayList<String>();
+
+            if (mGoogleApiClient.hasConnectedApi(Plus.API)) {
+                Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+                String name = currentPerson.getDisplayName();
+                String taglineStr = currentPerson.getTagline();
+                String uriStr = currentPerson.getImage().getUrl();
+                arrStr.add(name);       //index 0
+                arrStr.add(taglineStr); //index 1
+                arrStr.add(uriStr);     //index 2
+
+            }
+            userProfileIntent.putExtra("UserProfileActivity", arrStr);
+            SignInActivity.this.startActivity(userProfileIntent);
+            ///
+
+ /*           // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
             mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
 
-            ///
-            //profile picture initialization
-            final int PROFILE_PIC_SIZE = 100;
-            String personPhotoUrl = acct.getPhotoUrl().toString();
-            personPhotoUrl = personPhotoUrl.substring(0,
-                    personPhotoUrl.length() - 2)
-                    + PROFILE_PIC_SIZE;
+            //Tagline
+            if (mGoogleApiClient.hasConnectedApi(Plus.API)) {
+                Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+                String taglineStr = currentPerson.getTagline();
 
-            new LoadProfileImage(imageView).execute(personPhotoUrl);
+                if (taglineStr != null) {
+                    tagline.setText(getString(R.string.tagline, taglineStr));
+                }
+                else {
+                    tagline.setText(getString(R.string.tagline_na));
+                }
 
-            //TODO tagline initialization
+            }
 
-            ///
+            //Profile picture
+            final int PROFILE_PIC_SIZE = 500;
+            //Uri personPhotoUrl = acct.getPhotoUrl();
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            String uriStr = currentPerson.getImage().getUrl();
+            String shorter_url =
+                    uriStr.substring(0,
+                            uriStr.length() - 2)
+                            + PROFILE_PIC_SIZE;
+            new LoadProfileImage(imageView).execute(shorter_url);*/
+
+            /*if (acct.getPhotoUrl() != null) {
+                String personPhotoUrlString = personPhotoUrl.toString();
+                String shorter_url =
+                        personPhotoUrlString.substring(0,
+                                personPhotoUrlString.length() - 2)
+                        + PROFILE_PIC_SIZE;
+                System.out.println("shorter_url: " + shorter_url);
+                new LoadProfileImage(imageView).execute(shorter_url);
+            }
+            else imageView.setImageBitmap(null);*/
 
             updateUI(true);
         } else {
@@ -226,8 +443,6 @@ public class SignInActivity extends AppCompatActivity implements
         }
     }
 
-
-    ////
     /**
      * Background Async task to load user profile picture from url
      * */
@@ -255,6 +470,38 @@ public class SignInActivity extends AppCompatActivity implements
             bmImage.setImageBitmap(result);
         }
     }
+
+    /**
+     * Exception thrown when an error occurred while retrieving credentials.
+     */
+    public static class GetCredentialsException extends Exception {
+
+        protected String authorizationUrl;
+
+        /**
+         * Construct a GetCredentialsException.
+         *
+         * @param authorizationUrl The authorization URL to redirect the user to.
+         */
+        public GetCredentialsException(String authorizationUrl) {
+            this.authorizationUrl = authorizationUrl;
+        }
+
+        /**
+         * Set the authorization URL.
+         */
+        public void setAuthorizationUrl(String authorizationUrl) {
+            this.authorizationUrl = authorizationUrl;
+        }
+
+        /**
+         * @return the authorizationUrl
+         */
+        public String getAuthorizationUrl() {
+            return authorizationUrl;
+        }
+    }
+
 
 
 }
---
+++ app/src/main/java/cmpe277/project/skibuddy/UserProfileActivity.java
@@ -0,0 +1,89 @@
+package cmpe277.project.skibuddy;
+
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class UserProfileActivity extends Activity {
+    TextView name;
+    TextView tagline;
+    ImageView imageView;
+
+    ArrayList<String> arrStr = new ArrayList<String>();
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+
+        arrStr = getIntent().getStringArrayListExtra("UserProfileActivity");
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        name = (TextView) findViewById(R.id.name);
+        tagline = (TextView) findViewById(R.id.tagline);
+        imageView.setImageBitmap(null);
+
+        setUserProfile();
+    }
+
+    public void setUserProfile () {
+            //User name
+            name.setText(getString(R.string.signed_in_fmt, arrStr.get(0)));
+
+            //Tagline
+            String taglineStr = arrStr.get(1);
+
+            if (taglineStr != null) {
+                tagline.setText(getString(R.string.tagline, taglineStr));
+            } else {
+                tagline.setText(getString(R.string.tagline_na));
+            }
+
+            //Profile picture
+            final int PROFILE_PIC_SIZE = 500;
+            String uriStr = arrStr.get(2);
+            String shorter_url =
+                    uriStr.substring(0,
+                            uriStr.length() - 2)
+                            + PROFILE_PIC_SIZE;
+            new LoadProfileImage(imageView).execute(shorter_url);
+
+    }
+
+    /**
+     * Background Async task to load user profile picture from url
+     * */
+    private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public LoadProfileImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+}
